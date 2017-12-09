9c9f9f198be9814fe70dd195d812a6b276ba34d7
John Liman <johnliman@Johns-MacBook-Pro.local>
2017-12-10 01:16:15 +1100

    Added input text

--- src/main/java/com/aflmedia/app/App.java
+++ src/main/java/com/aflmedia/app/App.java
@@ -1,14 +1,25 @@
 package com.aflmedia.app;
 
-public class App 
+import java.util.Scanner;
+
+public class App
 {
     public static void main( String[] args )
     {
 
         try {
-            CircleGame game = new CircleGame(5, 2);
+
+            Scanner reader = new Scanner(System.in);  // Reading from System.in
+            System.out.println("Enter the number of children: ");
+            int n = reader.nextInt();
+
+            System.out.println("Enter the k value: ");
+            int k = reader.nextInt();
+
+            CircleGame game = new CircleGame(n, k);
             game.play();
 
+            System.out.println("The winning child is " + game.getWinningChild());
 
         } catch (AflMediaException e) {
             System.out.println(e.getMessage());
